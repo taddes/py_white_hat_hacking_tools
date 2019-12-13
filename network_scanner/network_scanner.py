aa3d2a5cf3aec039996dd0e768e7a9bc7f2f085c
Taddes Korris <taddes.korris@passportinc.com>
2019-12-13 13:54:19 -0500

    files

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -2,6 +2,25 @@ import scapy.all as scapy
 
 
 def scan(ip):
-    scapy.arping(ip)
+    # scapy.arping(ip)
+    arp_request = scapy.ARP(pdst=ip)
+    # Set broadcast mac
+    broadcast = scapy.Ether(sdt='ff:ff:ff:ff:ff:ff')
+    broadcast.show()
+    # scapy.ls(scapy.Ether())
+    # arp_request.pdst = ip
+    print(arp_request.summary())
+    print(broadcast.summary())
+    scapy.ls(scapy.ARP())
+    arp_request_broadcast = broadcast/arp_request
+    arp_request_broadcast.show()
 
 scan('10.0.2.1/24')
+
+# Ethernet Frame - data in networks send using mac address, a physical address engraved on each network card, and source mac and dest mac set in ethernet part of each packet.
+
+# Append ARP  request to Ethernet frame
+
+# Create a packet
+# Get ARP to ask who has target ip
+# Send request destination mac to broadcase MAC
